e93481f464274d792a9e181045cea45101c6062a
Mike McNeil <michael.r.mcneil@gmail.com>
2011-09-11 22:26:39 -0500

    stubble.

---
+++ stubble.js
@@ -0,0 +1,39 @@
+/**
+ * Stubble JS
+ * a trimmed moustache.js
+ * Mike McNeil, c.2011
+ * MichaelMcNeil.com
+ * Included here as a jquery extension
+ *
+ * Render a template string using a JSO
+ */
+$.stubble = function (template, valueMap) {
+    var result = template;
+    for (var key in valueMap) {
+        var re = new RegExp("\{\{"+escapeRegex(key)+"\}\}","g");
+        result = result.replace(re,valueMap[key]);
+    }
+    return result;
+
+    function escapeRegex (s) {
+        s = String(s === null ? "" : s);
+        return s.replace(/&(?!\w+;)|["'<>\\]/g, function(s) {
+            switch(s) {
+                case "&":
+                    return "&amp;";
+                case "\\":
+                    return "\\\\";
+                case '"':
+                    return '&quot;';
+                case "'":
+                    return '&#39;';
+                case "<":
+                    return "&lt;";
+                case ">":
+                    return "&gt;";
+                default:
+                    return s;
+            }
+        });
+    }
+}
